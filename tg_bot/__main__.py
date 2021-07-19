0ea3a3da1f84c0d96be9e9837320353766959650
TechyHarsith <86002583+TechyHarsith@users.noreply.github.com>
2021-07-19 09:38:20 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -58,7 +58,7 @@ DONATE_STRING = """Heya, glad to hear you want to donate!
 It took lots of work for [my creator](t.me/qatarpapabot) to get me to where I am now, and every donation helps \
 motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
 (see his bio!). He's just a poor student, so every little helps!
-There are two ways of paying him; [PayPal](paypal.me/TechyHarshith), or [Monzo](monzo.me/TechyHarshith)."""
+There are two ways of paying him; [PayPal](paypal.me/Harsithtechngaming?locale.x=en_GB), or [Monzo](monzo.me/TechyHarshith)."""
 
 IMPORTED = {}
 MIGRATEABLE = []
